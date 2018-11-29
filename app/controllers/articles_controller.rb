fcf9fbf9e06e03cbb039c8cb191dc654a2a5f0fa
Prem <upsei@ups.com>
2018-11-29 05:18:05 +0000

    Style changes in New App

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was succesfully created"
+      flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   def update
    
     if @article.update(article_params)
-      flash[:notice] = "Article was Scuccesully Updated"
+      flash[:success] = "Article was Scuccesully Updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] = "Article was Scuccesully Deleted"
+    flash[:success] = "Article was Scuccesully Deleted"
     redirect_to articles_path
   end
   
